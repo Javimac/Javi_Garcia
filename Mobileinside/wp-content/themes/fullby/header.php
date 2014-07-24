b0e36d6d2f29708edfb2cc83b715ed118d03a0be
Javimac <javigarciagonzalez@MacBook-Pro-de-Javi-Garcia-Gonzalez.local>
2014-07-24 11:58:13 +0200

    header

---
+++ Mobileinside/wp-content/themes/fullby/header.php
@@ -0,0 +1,264 @@
+<html lang="en-US" class="csstransforms csstransforms3d csstransitions"><head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Fullby</title>
+    <meta name="description" content="">
+    
+    <!-- Favicon -->
+    <link rel="icon" href="http://www.marchettidesign.net/fullby/demo/wp-content/themes/fullby1.3/img/favicon.png" type="image/x-icon"> 
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://www.marchettidesign.net/fullby/demo/wp-content/themes/fullby1.3/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://www.marchettidesign.net/fullby/demo/wp-content/themes/fullby1.3/font-awesome/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="http://www.marchettidesign.net/fullby/demo/wp-content/themes/fullby1.3/style.css" rel="stylesheet">
+  
+    
+    <!-- Google web Font -->
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,900,100" rel="stylesheet" type="text/css">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    
+    <!-- Analitics -->
+	    
+	<link rel="alternate" type="application/rss+xml" title="Fullby » Feed" href="http://www.marchettidesign.net/fullby/demo/?feed=rss2">
+<link rel="alternate" type="application/rss+xml" title="Fullby » Comments Feed" href="http://www.marchettidesign.net/fullby/demo/?feed=comments-rss2">
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.marchettidesign.net/fullby/demo/xmlrpc.php?rsd">
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.marchettidesign.net/fullby/demo/wp-includes/wlwmanifest.xml"> 
+<meta name="generator" content="WordPress 3.8.3">
+	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+ 
+	
+<script>window["_GOOG_TRANS_EXT_VER"] = "1";</script></head>
+<body class="home blog">
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+     
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainmenu">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <h1><a class="navbar-brand" href="http://www.marchettidesign.net/fullby/demo"><i class="fa fa-chevron-right"></i><i class="fa fa-chevron-right arrow"></i> Fullby</a></h1>
+        </div>
+        
+        <div id="mainmenu" class="collapse navbar-collapse">
+          <ul id="menu-main" class="nav navbar-nav navbar-right"><li id="menu-item-5" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-5"><a title="Home" href="http://www.marchettidesign.net/fullby/">Home</a></li>
+<li id="menu-item-4" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4"><a title="Sample Page" href="http://www.marchettidesign.net/fullby/demo/?page_id=2">Sample Page</a></li>
+</ul>        </div><!--/.nav-collapse -->
+    
+    </div>
+    
+        
+    	  
+    
+	    	 <div class="row featured">
+    
+										
+								  
+					    <div class="col-sm-4 col-md-4 item-featured">
+					    
+					    	
+							<a href="http://mobileinside.es/?p=8">
+				
+					    		<div class="caption">
+						    		<div class="cat"><span>Mobil</span></div>
+						    		<div class="date"><i class="fa fa-clock-o"></i> 28 Dec , 2013 &nbsp;
+						    		
+						    									             			
+						             		<i class="fa fa-video-camera"></i> Video
+						             			
+						             	
+						    		
+						    		</div>
+						    		
+						    		<h2 class="title">Video Post</h2>
+						    		
+					    		</div>
+
+				                					
+									 <img class="yt-featured" src="http://img.youtube.com/vi/XR3KWRDK3_U/hqdefault.jpg">
+										
+														    	
+						    </a>
+						
+						</div>
+					
+								  
+					    <div class="col-sm-4 col-md-4 item-featured">
+					    
+					    	
+							<a href="http://mobileinside.es/?p=5">
+				
+					    		<div class="caption">
+						    		<div class="cat"><span>What's New</span></div>
+						    		<div class="date"><i class="fa fa-clock-o"></i> 25 Jan , 2013 &nbsp;
+						    		
+						    					
+					             		
+						    		
+						    		</div>
+						    		
+						    		<h2 class="title">Featured Post</h2>
+						    		
+					    		</div>
+
+				                			
+			                        <img width="400" height="400" src="http://mobileinside.es/wp-content/uploads/2014/07/imgres-1.jpg" class="quad wp-post-image" alt="971377_10151355594456082_978282500_n">			                        				   
+			                    						    	
+						    </a>
+						
+						</div>
+					
+								  
+					    <div class="col-sm-4 col-md-4 item-featured">
+					    
+					    	
+							<a href="http://mobileinside.es/?p=8">
+				
+					    		<div class="caption">
+						    		<div class="cat"><span>Lifestyle</span></div>
+						    		<div class="date"><i class="fa fa-clock-o"></i> 10 Jan , 2013 &nbsp;
+						    		
+						    					
+					             		
+						    		
+						    		</div>
+						    		
+						    		<h2 class="title">Featured Post</h2>
+						    		
+					    		</div>
+
+				                			
+			                        <img width="400" height="400" src="http://mobileinside.es/wp-content/uploads/2014/07/iwatch-apple-.jpg" class="quad wp-post-image" alt="51">			                        				   
+			                    						    	
+						    </a>
+						
+						</div>
+					
+						
+				 		
+				</div>	
+				
+							
+		
+	<div class="navbar navbar-inverse navbar-sub">
+     
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#submenu">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          
+        </div>
+        
+        <div id="submenu" class="collapse navbar-collapse">
+          <ul id="menu-sub" class="nav navbar-nav"><li id="menu-item-6" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-6"><a title="What's New" href="http://www.marchettidesign.net/fullby/demo/?cat=1">What’s New</a></li>
+<li id="menu-item-42" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-42"><a title="Lifestyle" href="http://www.marchettidesign.net/fullby/demo/?cat=4">Lifestyle</a></li>
+<li id="menu-item-43" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-43"><a title="News" href="http://www.marchettidesign.net/fullby/demo/?cat=5">News</a></li>
+</ul>			
+			<div class="col-sm-3 col-md-3 pull-right search-cont">
+		        <form class="navbar-form" role="search" method="get" action="http://www.marchettidesign.net/fullby/demo">
+			        <div class="input-group">
+			            <input type="text" class="form-control" placeholder="Search" name="s" id="srch-term">
+			            <div class="input-group-btn">
+			                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+			            </div>
+			        </div>
+		        </form>
+	        </div>
+	        
+        </div><!--/.nav-collapse -->
+
+	</div>		
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="http://code.jquery.com/jquery-1.9.1.min.js" type="text/javascript"></script>
+<script src="http://www.marchettidesign.net/fullby/demo/wp-content/themes/fullby1.3/js/bootstrap.min.js"></script>
+<script src="http://www.marchettidesign.net/fullby/demo/wp-content/themes/fullby1.3/js/isotope.js"></script>
+   
+<script>
+(function ($) {
+	var $container = $('.grid'),
+		colWidth = function () {
+			var w = $container.width(), 
+				columnNum = 1,
+				columnWidth = 0;
+			if (w > 1200) {
+				columnNum  = 4;
+			} else if (w > 900) {
+				columnNum  = 3;
+			} else if (w > 600) {
+				columnNum  = 2;
+			} else if (w > 300) {
+				columnNum  = 1;
+			}
+			columnWidth = Math.floor(w/columnNum);
+			$container.find('.item').each(function() {
+				var $item = $(this),
+					multiplier_w = $item.attr('class').match(/item-w(\d)/),
+					multiplier_h = $item.attr('class').match(/item-h(\d)/),
+					width = multiplier_w ? columnWidth*multiplier_w[1]-10 : columnWidth-10,
+					height = multiplier_h ? columnWidth*multiplier_h[1]*0.5-40 : columnWidth*0.5-40;
+				$item.css({
+					width: width,
+					//height: height
+				});
+			});
+			return columnWidth;
+		},
+		isotope = function () {
+			$container.imagesLoaded( function(){
+				$container.isotope({
+					resizable: false,
+					itemSelector: '.item',
+					masonry: {
+						columnWidth: colWidth(),
+						gutterWidth: 20
+					}
+				});
+			});
+		};
+		
+	isotope();
+	
+	$(window).smartresize(isotope);
+	
+	//image fade
+	$('.item img').hide().one("load",function(){
+    	$(this).fadeIn(500);
+    }).each(function(){
+    	if(this.complete) $(this).trigger("load");
+    });
+    
+    //tab sidebar
+    $('#myTab a').click(function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	})
+
+	
+}(jQuery));
+
+
+</script>
+	
+	    
+  
+
+
+    		</body></html>
